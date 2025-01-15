1458753e432304e7bfa21918c3b3f2b5bb7f86e9
Ludmila Parshukova <lussia2809@yandex.ru>
2025-01-16 01:10:30 +0300

    Создание таблиц

--- init.sql
+++ init.sql
@@ -10,19 +10,23 @@ CREATE TABLE raw_data.sales(
     person_name varchar,
     phone varchar,
     discount INTEGER,
-    brand_origin varchar);
+    brand_origin varchar
+);
 
 /* Заполните таблицу sales данными, используя команду \copy в psql. */
 \copy raw_data.sales(id,auto,gasoline_consumption,price,date,person_name,phone,discount,brand_origin) FROM 'C:\cars.csv' CSV HEADER NULL 'null';
 
+
+/* Создайте схему car_shop, а в ней cоздайте нормализованные таблицы. */
 CREATE schema car_shop;
 
 CREATE TABLE car_shop.clients(
 	id SERIAL PRIMARY KEY, /* Автоматическое присваивание и увеличение id. */
     client_name VARCHAR NOT NULL, /* Занимает меньше оперативной памяти. */
-    phone VARCHAR NOT NULL UNIQUE); /* Может содержать цифры, буквы, символы. */
+    phone VARCHAR NOT NULL unique /* Может содержать цифры, буквы, символы. */
+); 
 
-CREATE TABLE car_shop.cars_colors (
+CREATE TABLE car_shop.colors (
     id SERIAL PRIMARY KEY, /* Автоматическое присваивание и увеличение id. */
     name VARCHAR NOT NULL UNIQUE /* Занимает меньше оперативной памяти. */
 );
@@ -36,8 +40,24 @@ CREATE TABLE car_shop.cars (
     id SERIAL PRIMARY KEY, /* Автоматическое присваивание и увеличение id. */
     brand_name VARCHAR NOT NULL, /* Занимает меньше оперативной памяти. */
     model_name VARCHAR NOT NULL, /* Может содержать цифры, буквы, символы. */
-    gasoline_consumption NUMERIC(3, 1), /* При различных операциях с данными дробные числа не потеряются. */
+    gasoline_consumption NUMERIC, /* Повышенная точность. При различных операциях с данными дробные числа не потеряются. */
     country_id INTEGER REFERENCES car_shop.countries(id), /* Согласование с первичным ключом. */
     UNIQUE (brand_name, model_name)
 );
+
+CREATE TABLE car_shop.cars_colors (
+    auto_id INTEGER REFERENCES car_shop.cars(id) NOT NULL, /* Для согласования с первичным ключом. */
+    color_id INTEGER REFERENCES car_shop.colors(id) NOT NULL, /* Для согласования с первичным ключом. */
+    PRIMARY KEY(auto_id, color_id)
+);
+
+CREATE TABLE car_shop.sales (
+    id SERIAL PRIMARY KEY, /* Автоматическое присваивание и увеличение id. */
+    price NUMERIC NOT NULL, /* Повышенная точность. */
+    date DATE NOT NULL, /* Дата для даты, все логично :з */
+    discount SMALLINT, /* Экономит память, значение не превысит пределов. */
+    auto_id INTEGER REFERENCES car_shop.cars(id) NOT NULL, /* Для согласования с первичным ключом. */
+    client_id INTEGER REFERENCES car_shop.clients(id) NOT NULL /* Для согласования с первичным ключом.*/
+);
+
 
