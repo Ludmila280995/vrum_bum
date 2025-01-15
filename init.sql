e4a165e656d862d4987d9a6e5755d87a4c85e5a3
Ludmila Parshukova <lussia2809@yandex.ru>
2025-01-16 00:13:36 +0300

    Обновила столбцы таблицы

--- init.sql
+++ init.sql
@@ -2,10 +2,15 @@
 CREATE SCHEMA raw_data;
 
 CREATE TABLE raw_data.sales(
-id SERIAL PRIMARY KEY,
-name TEXT,
-phone TEXT,
-city TEXT);
+    id SERIAL PRIMARY KEY,
+    auto varchar,
+    gasoline_consumption NUMERIC,
+    price NUMERIC,
+    date DATE,
+    person_name varchar,
+    phone varchar,
+    discount INTEGER,
+    brand_origin varchar);
 
 /* Заполните таблицу sales данными, используя команду \copy в psql. */
-\copy raw_data.sales(id, name, phone, city) FROM 'C:\clients.csv' CSV HEADER NULL 'null';
+\copy raw_data.sales(id,auto,gasoline_consumption,price,date,person_name,phone,discount,brand_origin) FROM 'C:\cars.csv' CSV HEADER NULL 'null';
